33c46b9200dc36e10da60088eb8db5af42308533
babigdk <babigdk.com>
2026-06-19 13:44:44 -0700

    fixing the missing import

--- meridian-api/src/post/provider/post.service.ts
+++ meridian-api/src/post/provider/post.service.ts
@@ -1,4 +1,5 @@
 import { Inject, Injectable } from '@nestjs/common';
+import { HttpException, HttpStatus } from "@nestjs/common";
 import { InjectRepository } from '@nestjs/typeorm';
 import { Repository } from 'typeorm';
 
